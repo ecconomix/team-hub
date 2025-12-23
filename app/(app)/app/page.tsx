002771447d9ae9f31821dc7eb074f080b477c459
ochechkevych <ochechkevych@cargurus.com>
2025-12-23 15:47:21 +0200

    refactor: Restructure application layout and pages, removing old Home component and adding new layout and page components for app and public sections

---
+++ app/(app)/app/page.tsx
@@ -0,0 +1,7 @@
+export default function AppPage() {
+  return (
+    <main>
+      <h1>App page</h1>
+    </main>
+  );
+}
---
+++ app/(public)/page.tsx
@@ -0,0 +1,7 @@
+export default function PublicPage() {
+  return (
+    <main>
+      <h1>Landing page</h1>
+    </main>
+  );
+}
